00e351e1b8c39c5edf08c11ff94323ffc2983b15
Michael Delcambre <delcambrem@gmail.com>
2022-02-02 22:21:36 -0600

    done

---
+++ app/serializers/author_serializer.rb
@@ -0,0 +1,8 @@
+class AuthorSerializer < ActiveModel::Serializer
+  attributes :name
+  has_one :profile
+  has_many :posts, serializer: AuthorPostSerializer
+  # has_many :tags, through: :posts
+end
+
+
